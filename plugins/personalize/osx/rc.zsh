b95306739b4b70db5b90388841ebf607e7bb5524
Ty Alexander <ty.alexander@sendgrid.com>
2018-10-29 18:22:32 -0600

    Added support for command not found lookups in brew

--- plugins/personalize/osx/rc.zsh
+++ plugins/personalize/osx/rc.zsh
@@ -5,3 +5,7 @@
 # This file loads Ty's OSX zsh run control.
 #
 #############################################################################
+
+# command-not-found
+if brew command command-not-found-init > /dev/null 2>&1; then eval "$(brew command-not-found-init)"; fi
+
