e3639cf503083fd4d1b6b8e480b043c94cd947fd
Ty Alexander <ty.alexander@sendgrid.com>
2018-11-18 07:31:46 -0700

    update completion

--- plugins/personalize/setups/osx/rc.zsh
+++ plugins/personalize/setups/osx/rc.zsh
@@ -5,7 +5,5 @@
 # This file loads Ty's OSX zsh run control.
 #
 #############################################################################
-
 # command-not-found
 if brew command command-not-found-init > /dev/null 2>&1; then eval "$(brew command-not-found-init)"; fi
-
--- plugins/zsh-completions/zsh-completions.plugin.zsh
+++ plugins/zsh-completions/zsh-completions.plugin.zsh
@@ -1,1 +1,8 @@
+export COMPLETION_WAITING_DOTS="true"
+export DISABLE_AUTO_TITLE="true"
+export DISABLE_CORRECTION="true"
+
 fpath=(/usr/local/share/zsh-completions $fpath)
+
+# https://github.com/zsh-users/zsh-completions#oh-my-zsh
+autoload -U compinit && compinit
